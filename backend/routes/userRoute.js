7d6a85d0fa03bd83d5d5023766693ce6f12a3431
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-05 11:31:05 -0500

    Minor change

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { authUser, getUserProfile, logoutUser, registerUser, UpdateUserProfile } from "../controller/userController.js"
+import { authUser, getUserProfile, logoutUser, registerUser, updateUserProfile,  } from "../controller/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
@@ -7,6 +7,6 @@ const router = express.Router()
 router.route("/auth").post(authUser)
 router.route("/").post(registerUser)
 router.route("/logout").post(logoutUser)
-router.route("/profile").get(protect,getUserProfile).put(protect,UpdateUserProfile)
+router.route("/profile").get(protect,getUserProfile).put(protect,updateUserProfile)
 
 export default router
